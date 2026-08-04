f66c7837d0673cd021ce9d4fad92fa8133309195
Vidal <vitor.vidal06@aluno.ifce.edu.br>
2026-08-03 21:41:48 -0300

    adiciona o exemplo trabalhado nas aulas de 28/07 e 03/08

---
+++ Exmplos/meuscript.js
@@ -0,0 +1,21 @@
+ console.log("Hello, World");
+
+ //console. log(idade);
+
+ //criando bloco de codigo com chaves {}
+ if (true) {
+    var teste = "hahahhaha";
+ }
+console.log(teste);
+
+ let idade = 20;
+ let nome = "VIDAL";
+ console.log("idade do individo é: " + idade)
+
+ // Seleciona um elemento pelo seu ID
+ document.getElementById("pnome").innerHTML = nome + " tem " + idade + " anos de idade ";
+ const PI = 3.14;
+
+ let ultimo_nome; //Snake case
+ let primeiroNome; //camal case (camelo)
+ let PrimeiroNome; // Pastal case
